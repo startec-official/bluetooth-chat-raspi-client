a51dd0e0a8e2e8947466cb1c99bcb3be96c4a084
startec-official <gponon2000@gmail.com>
2021-03-27 09:23:01 -0400

    added dummy send

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-chat-demo';
+  public messages: any[];
+
+  ngOnInit(): void {
+    this.messages = [
+      { msg: 'hello there', self: false },
+      { msg: 'oh hi', self: true }
+    ];
+  }
+
+  addMessage(newMessage: string) {
+    this.messages.push({msg: newMessage, self: true});
+  }
 }
--- src/app/utils/data.service.ts
+++ src/app/utils/data.service.ts
@@ -1,14 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private socket: Socket) { }
 
-  recieveData() {
-    
+  getMessage() {
+    return this.socket.fromEvent('message').pipe(map((data:any) => data.msg));
   }
 }
